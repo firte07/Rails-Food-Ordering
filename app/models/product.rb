8e23f791de8f3ba4bdfe0cc49d99553007e1980c
firte07 <firte.catalin@gmail.com>
2021-05-04 11:26:31 +0300

    Rename images, add classes to all html native elements

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   validates :title, length: { maximum: 50 }, presence: true
-  validates :description, length: { maximum: 50 }, presence: true
+  validates :description, length: { maximum: 150 }, presence: true
   validates :price, presence: true, numericality: { only_float: true, greater_than: 0 }
   has_one_attached :image
 end

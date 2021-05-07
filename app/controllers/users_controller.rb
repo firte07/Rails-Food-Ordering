412e89f4968ffc13d758115ca2e3110a92e40715
firte07 <firte.catalin@gmail.com>
2021-05-07 15:49:34 +0300

    Create login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the best restaurant!"
-      redirect_to @user
+      redirect_to login_path
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'main#welcome'
   get 'users/new'
   get 'main/welcome'
   get 'pages/home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
   resources :products
   resources :users
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end

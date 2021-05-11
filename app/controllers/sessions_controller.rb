944a720c6926da32655d9fa31123ae26b71fbbdb
firte07 <firte.catalin@gmail.com>
2021-05-11 14:26:34 +0300

    Put end curly bracket on a new line in all scss files and remove empty spaces

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
 
-  def new ; end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
       reset_session
       log_in user

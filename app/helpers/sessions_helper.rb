7d10a577cc0bef8dcab1caad981098fc6562e5b7
firte07 <firte.catalin@gmail.com>
2021-05-18 13:49:09 +0300

    Change an if for authenticated and also change the env file for cloudinary + change image_tag to cl_image_tag

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,7 +38,7 @@ module SessionsHelper
 
   def log_in_user(user_id)
     user = User.find_by(id: user_id)
-    if user&.user.authenticated?(cookies[:remember_token])
+    if user&.authenticated?(cookies[:remember_token])
       log_in(user)
       @current_user = user
     end
